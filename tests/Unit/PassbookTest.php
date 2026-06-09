cc254f0917ef547b663505cd80858ccfaf435453
LauLaman <8283992+LauLaman@users.noreply.github.com>
2026-06-09 13:06:10 +0200

    Add new Passbook base properties and URL support

--- tests/Unit/PassbookTest.php
+++ tests/Unit/PassbookTest.php
@@ -228,7 +228,7 @@ final class PassbookTest extends TestCase
 
         $barcode =  new Barcode();
         $barcode->setMessage('barcode');
-        $barcode->setFormat(BarcodeFormat::code128());
+        $barcode->setFormat(BarcodeFormat::Code128);
 
         $passbook->setBarcode($barcode);
 
@@ -236,7 +236,7 @@ final class PassbookTest extends TestCase
         self::assertArrayHasKey('barcode', $data);
 
         $expectedBarcodeData = [
-            'format' => BarcodeFormat::code128()->getValue(),
+            'format' => BarcodeFormat::Code128->value,
             'message' => 'barcode',
             'messageEncoding' => 'iso-8859-1',
         ];
