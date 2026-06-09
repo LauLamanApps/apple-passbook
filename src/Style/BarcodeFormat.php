fa8103e2f8ef4ed4c89b21809eb84e06b0a073a5
LauLaman <8283992+LauLaman@users.noreply.github.com>
2026-06-09 13:05:46 +0200

    Convert to native PHP enums

--- src/Style/BarcodeFormat.php
+++ src/Style/BarcodeFormat.php
@@ -4,22 +4,14 @@ declare(strict_types=1);
 
 namespace LauLamanApps\ApplePassbook\Style;
 
-use Werkspot\Enum\AbstractEnum;
-
-/**
- * @method static self qr()
- * @method bool isQr()
- * @method static self pdf417()
- * @method bool isPdf417()
- * @method static self aztec()
- * @method bool isAztec()
- * @method static self code128()
- * @method bool isCode128()
- */
-class BarcodeFormat extends AbstractEnum
+enum BarcodeFormat: string
 {
-    private const QR = 'PKBarcodeFormatQR';
-    private const PDF417 = 'PKBarcodeFormatPDF417';
-    private const Aztec = 'PKBarcodeFormatAztec';
-    private const Code128 = 'PKBarcodeFormatCode128';
+    case Qr = 'PKBarcodeFormatQR';
+    case Pdf417 = 'PKBarcodeFormatPDF417';
+    case Aztec = 'PKBarcodeFormatAztec';
+    case Code128 = 'PKBarcodeFormatCode128';
+    case Code39 = 'PKBarcodeFormatCode39';
+    case Codabar = 'PKBarcodeFormatCodabar';
+    case Ean13 = 'PKBarcodeFormatEAN13';
+    case I2of5 = 'PKBarcodeFormatI2of5';
 }
